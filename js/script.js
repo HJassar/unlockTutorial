5397d54f9334cd498a0a2c0d668134461a53acd6
Hamza Jassar <jassarhamza@gmail.com>
2021-03-26 22:57:29 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// 🔓
+const correctComb = '1234'
+
+// 🏢 DOM
+const DOM_main = document.querySelector('#DOM_main')
+const DOM_lockImg = document.querySelector('#DOM_lockImg')
+const DOM_correctCombo = document.querySelector('#DOM_correctCombo')
+
+// 🏁 Start here
