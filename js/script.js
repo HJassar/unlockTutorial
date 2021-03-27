7e00255d074f7d37195b429d2c7bbf667a8291e8
Hamza Jassar <jassarhamza@gmail.com>
2021-03-26 23:31:10 -0500

    start point of tutorial

--- js/script.js
+++ js/script.js
@@ -5,5 +5,6 @@ const correctComb = '1234'
 const DOM_main = document.querySelector('#DOM_main')
 const DOM_lockImg = document.querySelector('#DOM_lockImg')
 const DOM_correctCombo = document.querySelector('#DOM_correctCombo')
+const DOM_comboDigits = document.querySelectorAll('.DOM_comboDigit')
 
 // 🏁 Start here
